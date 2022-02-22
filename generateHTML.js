0ad793bd54ee5d68d8ff8744bff9ae1716cd2caa
NotANewt <meegafauna@gmail.com>
2022-02-21 18:57:51 -0600

    basic functionality to prompt manager info send to generatehtml and generate web page with manager card

--- generateHTML.js
+++ generateHTML.js
@@ -1,9 +1,71 @@
 /*
+ generateManager
+  generates what will go in the Manager Card
+    * takes in answers and data from manager.js, engineer.js, and intern.js
+    * uses a template literal for the layout of the HTML file with the supplied answers added into the template
+    * returns the template literal
+*/
+function generateManager() {
+  console.log("here is a manager!");
+}
+
+/*
  generateHTMLText
   generates what will go in the HTML file
     * takes in answers and data from manager.js, engineer.js, and intern.js
     * uses a template literal for the layout of the HTML file with the supplied answers added into the template
     * returns the template literal
 */
+function generateHTMLText({ managerName, managerId, managerEmail, managerOfficeNumber, managerRole }) {
+  return `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Profile</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/49aab69f62.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+    <!-- header -->
+    <header>
+        <h1>My Team</h1>
+    </header>
+
+    <div class="container">
+
+        <!-- Manager sample card -->
+        <div class="card">
+
+            <div class="card_top">
+                <p class="name">${managerName}</p>
+                <p class="employeeType"><i class="fa-solid fa-mug-hot"></i> ${managerRole}</p>
+            </div>
+
+            <div class="card_info">
+                <p class="card_id">ID: ${managerId}</p>
+                <p class="card_email">Email: <a href="mailto:${managerEmail}">${managerEmail}</a>
+                </p>
+                <p class="card_officeNumber">Office Number: ${managerOfficeNumber}</p>
+            </div>
+        </div>   
+    </div>
+
+    <!-- footer -->
+    <footer class="">
+        <p class="">Made with &#9825; and OOP by <a class="no-text-decor" href="https://github.com/NotANewt"
+                target="_blank">Meegan</a></p>
+    </footer>
+</body>
+
+</html>`;
+}
 
 // modules to export
+module.exports = { generateHTMLText };
--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-// links to required files and packages
+// Packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateHTML = require("./generateHTML.js");
+const employee = require("./lib/employee");
+const engineer = require("./lib/engineer");
+const intern = require("./lib/intern");
+const manager = require("./lib/manager");
 
 /*
     array of questions objects for Inquirer
@@ -26,6 +33,31 @@
         finish building team
             exits application
 */
+const questions = [
+  {
+    name: "name",
+    type: "input",
+    message: "Enter your team Manager's name.",
+  },
+
+  {
+    name: "id",
+    type: "input",
+    message: "Enter your team Manager's employee ID number.",
+  },
+
+  {
+    name: "email",
+    type: "input",
+    message: "Enter your team Manager's email.",
+  },
+
+  {
+    name: "officeNumber",
+    type: "input",
+    message: "Enter your team Manager's office number.",
+  },
+];
 
 /*
     runInquirer
@@ -40,24 +72,47 @@
             const HTMLText = generateHTMLText.methodName(answers, managerCard, engineerCard, internCard)
 
 */
+function runInqurer() {
+  inquirer.prompt(questions).then((answers) => {
+    const employeeManager = new manager(answers);
+
+    const managerName = employeeManager.name;
+    const managerId = employeeManager.id;
+    const managerEmail = employeeManager.email;
+    const managerOfficeNumber = employeeManager.officeNumber;
+    const managerRole = employeeManager.getRole();
+
+    console.log(managerName);
+    console.log(managerId);
+    console.log(managerEmail);
+    console.log(managerOfficeNumber);
+    console.log(managerRole);
+
+    const HTMLText = generateHTML.generateHTMLText({ managerName, managerId, managerEmail, managerOfficeNumber, managerRole });
+    writeToFile(HTMLText);
+  });
+}
 
 /*
- Write README file
-  writes the README file using "HTMLText" from generateHTMLText.js
+ Write HTML file
+  writes the index.html file using "HTMLText" from generateHTMLText.js
     * titles file "index.html"
     * writes file using "HTMLText" from generateHTMLText.js
     * if there is an error, console error
     * if the file is written successfully, console log sucess.
 */
+function writeToFile(HTMLText) {
+  fs.writeFile("./dist/index.html", HTMLText, (err) => (err ? console.error(err) : console.log("Success! The HTML webpage has been genarated.")));
+}
 
 /*
  init()
   initializatize the application
     * calls runInquirer function
 */
-// function init() {
-//     runInqurer();
-//   }
+function init() {
+  runInqurer();
+}
 
 // Initialize the application by calling init function
-//   init();
+init();
